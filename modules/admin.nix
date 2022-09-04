69b5b2914fea0479592faee16c3c418f3a044c03
TEC <git@tecosaur.net>
2022-09-05 02:30:53 +0800

    Set password for admin user

--- modules/admin.nix
+++ modules/admin.nix
@@ -3,7 +3,7 @@
 {
   users.users.admin = {
     name = "admin";
-    initialPassword = "1234";
+    hashedPassword = "$6$ET8BLqODvw77VOmI$oun2gILUqBr/3WonH2FO1L.myMIM80KeyO5W1GrYhJTo./jk7XcG8B3vEEcbpfx3R9h.sR0VV187/MgnsnouB1";
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOZZqcJOLdN+QFHKyW8ST2zz750+8TdvO9IT5geXpQVt tec@tranquillity" ];
