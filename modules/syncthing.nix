2eb2bf27dde60a776798063d66ccd6978d1f88ad
TEC <git@tecosaur.net>
2023-12-16 09:31:26 +0800

    Add laptop as a syncthing device

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -16,12 +16,13 @@ with lib;
       };
       devices = {
         "tranquillity" = { id = "VXWXMXK-MWENVPV-PV75JQH-45OP44F-QMPH645-JVWGJB2-C2GKHSV-QARV5A2"; };
+        "demure"       = { id = "DLCN7FP-BMHTAG6-QZHYGPJ-74IALUD-NZII77W-K73SPGU-L7QNGIS-NO674AH"; };
         "phone"        = { id = "IMNPYY2-BZMILMV-PYUCUOS-UCO4WNJ-UBRW7EY-VESRBGA-XHTNZ6G-E34J5QC"; };
       };
       folders = {
         "tec-public" = {
           path = "~/public";
-          devices = [ "tranquillity" "phone" ];
+          devices = [ "tranquillity" "demure" "phone" ];
           type = "receiveonly";
         };
       };
