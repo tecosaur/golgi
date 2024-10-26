b76a98769add7daeae7304ad1e39d916031e0d5a
TEC <git@tecosaur.net>
2024-10-26 15:26:35 +0800

    Filter out auth-only actions on unauth paste pages

--- modules/pastebin.nix
+++ modules/pastebin.nix
@@ -3,6 +3,32 @@
 let
   paste-domain = "pastes.${config.globals.domain}";
   page-name = "μPaste";
+in
+let
+  caddy-unauth-filter =
+    ''
+    @unauth-html {
+        not header Cookie *authelia_session*
+        path /upload/* /p/*
+    }
+    replace @unauth-html {
+        re "<div id=\"nav\"[\s\S]*?</div>[\s\S]*?" ""
+        re `<div style="float: left">\s*(?:(<button[^>]*>[\s\S]+?<\/button>(?:\s*<button[^>]*>[^>]+?<\/button>)?)\s*(<a[^>]+>Raw)[^<]+(<\/a>))?[\s\S]*?<\/div>` <<HTML
+          <div style="float: left">
+          <b style="margin-right: 1.5rem">
+            <a href="https://${paste-domain}">
+              <img width=100 style="margin-bottom: -6px; margin-right: 0.5rem;" src="https://${paste-domain}/static/logo.png"
+                  onload="this.parentElement.href = 'https://${config.globals.auth-domain}/?rd=' + encodeURIComponent(window.location.href) + '&rm=GET';">
+            </a>
+            ${config.services.microbin.settings.MICROBIN_TITLE}
+          </b>
+          $1$2$3
+          </div>
+          HTML
+        re "\n*<div>\s*<p[^>]*>Read[^<]+<\/p>\s*<\/div>" ""
+        `<a href="https://${paste-domain}/"> Go Home</a>` ""
+    }
+    '';
 in {
   services.microbin = {
     enable = true;
@@ -30,13 +56,18 @@ in {
 
   services.caddy.virtualHosts."${paste-domain}".extraConfig =
     ''
-    @public path /upload/* /raw/* /u/* /qr/* /static/*
+    @public path /static/* /upload/* /file/* /p/* /raw/* /u/* /qr/* /auth/* /auth_file/*
     route @public {
-        reverse_proxy :${toString config.services.microbin.settings.MICROBIN_PORT}
+        ${caddy-unauth-filter}
+        reverse_proxy :${toString config.services.microbin.settings.MICROBIN_PORT} {
+            header_up Accept-Encoding identity
+        }
     }
     route * {
         import auth
-        reverse_proxy :${toString config.services.microbin.settings.MICROBIN_PORT}
+        reverse_proxy :${toString config.services.microbin.settings.MICROBIN_PORT} {
+            header_up Accept-Encoding identity
+        }
     }
     '';
 
