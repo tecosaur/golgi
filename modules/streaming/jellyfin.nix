126bf57d042aa2008b6f743938a49bdef39b8cc5
TEC <git@tecosaur.net>
2025-09-04 22:44:50 +0800

    Jellyfin OIDC config + theming

--- modules/streaming/jellyfin.nix
+++ modules/streaming/jellyfin.nix
@@ -3,19 +3,30 @@
 # Look at: <https://github.com/Sveske-Juice/declarative-jellyfin>
 # Also see: <https://github.com/9p4/jellyfin-plugin-sso/issues/16#issuecomment-2953811762>
 
+# Then also (https://gitlab.com/DomiStyle/jellysearch):
+# - <https://git.vimium.com/jordan/nix-config/src/branch/master/pkgs/jellysearch>
+# - <https://git.vimium.com/jordan/nix-config/src/branch/master/hosts/library/jellysearch.nix>
+
 let
   media-dir = "/data/media";
   movies-dir = "${media-dir}/movies";
   shows-dir = "${media-dir}/shows";
+  anime-dir = "${media-dir}/anime";
   books-dir = "${media-dir}/books";
   music-dir = "${media-dir}/music";
   jellyfin-domain = "${config.site.apps.jellyfin.subdomain}.${config.site.domain}";
 in {
   site.apps.jellyfin.enabled = true;
 
+  age.secrets.jellyfin-oidc = {
+    owner = "jellyfin";
+    group = "users";
+    file = ../../secrets/jellyfin-oidc-secret.age;
+  };
+
   services.declarative-jellyfin = {
     enable = true;
-    serverId = "0mdg8vydu0b46asdj6r28hdwkh2bi7zj";
+    serverId = "f2d18e8c67994ce1baa11016df427c9f";
     network = {
       enableIPv6 = true;
       enableHttps = false; # Handled by Caddy
@@ -40,16 +51,25 @@ in {
       ];
     };
     system = {
+      serverName = "Media";
       trickplayOptions = {
         enableHwAcceleration = true;
         enableHwEncoding = true;
         enableKeyFrameOnlyExtraction = true;
         processThreads = 2;
       };
+      pluginRepositories = [
+        {
+          tag = "RepositoryInfo";
+          content = {
+            Name = "Jellyfin Stable";
+            Url = "https://repo.jellyfin.org/files/plugin/manifest.json";
+          };
+        }
+      ];
     };
     libraries = {
       Movies = {
-        enabled = true;
         contentType = "movies";
         pathInfos = [ movies-dir ];
         typeOptions.Movies = {
@@ -64,19 +84,23 @@ in {
         };
       };
       Shows = {
-        enabled = true;
         contentType = "tvshows";
         pathInfos = [ shows-dir ];
+        enableAutomaticSeriesGrouping = true;
+      };
+      Anime = {
+        contentType = "tvshows";
+        pathInfos = [ anime-dir ];
+        enableAutomaticSeriesGrouping = true;
       };
       Books = {
-        enabled = true;
         contentType = "books";
         pathInfos = [ books-dir ];
       };
       Music = {
-        enabled = true;
         contentType = "music";
         pathInfos = [ music-dir ];
+        customTagDelimiters = [ "," "/" "|" ";" ];
       };
     };
     users = {
@@ -89,6 +113,60 @@ in {
       };
     };
   };
+
+  # Declarative Jellyfin replaces the `ExecStart` of the `jellyfin`
+  # service with a wrapper script, which means we can safely claim
+  # `preStart` ourselves.
+  systemd.services.jellyfin.preStart = let
+    branding-xml =
+      ''
+      <?xml version="1.0" encoding="utf-8"?>
+      <BrandingOptions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+        <LoginDisclaimer>&lt;form action="https://${jellyfin-domain}/sso/OID/start/authelia"&gt;
+        &lt;button class="raised block emby-button button-submit"&gt;
+          Sign in
+        &lt;/button&gt;
+      &lt;/form&gt;</LoginDisclaimer>
+        <CustomCss>${builtins.replaceStrings [ "&" "<" ">" ] [ "&amp;" "&lt;" "&gt;" ]
+          (lib.trim (builtins.readFile ./jellyfin.css))}</CustomCss>
+        <SplashscreenEnabled>false</SplashscreenEnabled>
+      </BrandingOptions>
+      '';
+    branding-file = pkgs.writeText "jellyfin-branding.xml" branding-xml;
+    branding-path = "${config.services.jellyfin.configDir}/branding.xml";
+    sso-template = builtins.replaceStrings [ "#oidc_endpoint#" ]
+      [ "https://${config.site.apps.authelia.subdomain}.${config.site.domain}" ]
+      (builtins.readFile ./jellyfin-sso-template.xml);
+    sso-file = pkgs.writeText "jellyfin-sso.xml" sso-template;
+    sso-path = "${config.services.jellyfin.dataDir}/plugins/configurations/SSO-Auth.xml";
+    replaceSecretBin = lib.getExe pkgs.replace-secret;
+    xmlstarlet = lib.getExe pkgs.xmlstarlet;
+  in
+    ''
+    umask 007
+    mkdir -p "$(dirname '${branding-path}')"
+    cp -rf '${branding-file}' '${branding-path}'
+    mkdir -p '${config.services.jellyfin.configDir}/plugins/configurations'
+    # Preserve folder settings that cannot (easily) be managed declaratively
+    if [ -f '${sso-path}' ]; then
+        JELLY_OIDC_ENABLED_FOLDERS="$(${xmlstarlet} sel -t -c '//PluginConfiguration/OidConfigs[1]/item/value/PluginConfiguration/EnabledFolders' '${sso-path}' | sed '1d;$d')"
+        JELLY_OIDC_FOLDER_ROLE_MAPPINGS="$(${xmlstarlet} sel -t -c '//PluginConfiguration/OidConfigs[1]/item/value/PluginConfiguration/FolderRoleMappings' '${sso-path}' | sed '1d;$d')"
+    else
+        JELLY_OIDC_ENABLED_FOLDERS=""
+        JELLY_OIDC_FOLDER_ROLE_MAPPINGS=""
+    fi
+    mkdir -p "$(dirname '${sso-path}')"
+    install -m 600 '${sso-file}' '${sso-path}'
+    ${replaceSecretBin} '#oidc_secret#' '${config.age.secrets.jellyfin-oidc.path}' '${sso-path}'
+    sed -i '/#enabled_folders#/{
+      r /dev/stdin
+      d
+    }' '${sso-path}' <<<"$JELLY_OIDC_ENABLED_FOLDERS"
+    sed -i '/#folder_role_mappings#/{
+      r /dev/stdin
+      d
+    }' '${sso-path}' <<<"$JELLY_OIDC_FOLDER_ROLE_MAPPINGS"
+    '';
 
   users.users.${config.services.jellyfin.user}.extraGroups = ["video" "render"];
 
@@ -104,15 +182,16 @@ in {
     "d ${media-dir} 0755 root users - -"
     "d ${movies-dir} 0775 ${config.services.jellyfin.user} users - -"
     "d ${shows-dir} 0775 ${config.services.jellyfin.user} users - -"
-    "d ${books-dir} 0575 ${config.services.jellyfin.user} users - -"
+    "d ${anime-dir} 0775 ${config.services.jellyfin.user} users - -"
+    # "d ${books-dir} 0575 ${config.services.jellyfin.user} users - -"
     "d ${music-dir} 0575 ${config.services.jellyfin.user} users - -"
   ];
 
   services.caddy.virtualHosts."${config.site.apps.jellyfin.subdomain}.${config.site.domain}".extraConfig =
     ''
-    handle /web/assets/img/icon-transparent.png {
-        uri strip_prefix /web/assets/img
+    handle_path /web/assets/img/* {
         root * ${../../assets/jellyfin}
+        try_files {path} {http.request.uri}
         file_server
     }
     handle /web/*.ico {
