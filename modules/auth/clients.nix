cff28b22d9cf7bd1ef56b16d4f59e68d972bc958
TEC <git@tecosaur.net>
2025-09-21 19:41:17 +0800

    Relocate OIDC client secrets to files

--- modules/auth/clients.nix
+++ modules/auth/clients.nix
@@ -2,13 +2,15 @@
 
 let
   mkClient = app: overrides@{
-    client_secret,
+    client_secret ? null,
     redirect_paths ? null,
     redirect_uris ? null,
     force ? false,
     ...
   }:
     let
+      readSecret = name:
+        lib.removeSuffix "\n" (builtins.readFile (../../secrets + "/${name}-oidc-secret.txt"));
       pathsToUris = ps:
         map (p: "https://${app.subdomain}.${config.site.domain}/${p}") ps;
       oidcRedirects = (if redirect_paths != null then
@@ -19,7 +21,8 @@ let
         client_id = safeName;
         client_name = app.name;
         authorization_policy = safeName;
-        client_secret = client_secret;
+        client_secret = if client_secret != null then
+          client_secret else readSecret safeName;
         public = false;
         consent_mode = "implicit";
         require_pkce = true;
@@ -133,22 +136,18 @@ in {
       };
       clients = lib.flatten [
         (mkClient config.site.apps.calibre-web {
-          client_secret = "$argon2id$v=19$m=65536,t=3,p=4$w8Z9XPzJzP+ZM4DvPZ2EyA$KsUuWk7eE/MnHc/YaFDLuNsWeKHYA/SbQ2AfpSijfuo";
           redirect_paths = [ "login/generic/authorized" ];
           require_pkce = false;
           pkce_challenge_method = "";
           authorization_policy = "one_factor";
         })
         (mkClient config.site.apps.forgejo {
-          client_secret = "$argon2id$v=19$m=65536,t=3,p=4$fRdkE7fHqAPkVQYXn1Zksw$O6WQ4fsNoN/0vzOK4hT1oreVPyFoVcK2hOIFx3axe/A";
           redirect_paths = [ "user/oauth2/authelia/callback" ];
         })
         (mkClient config.site.apps.headscale {
-          client_secret = "$argon2id$v=19$m=65536,t=3,p=4$JxZLRd3W145f3uB3D2UVqw$kJVGMuaLzESu9kWDYE8p8mnM2qRRAiaLgAI0vJaCu5k";
           redirect_paths = [ "oidc/callback" "admin/oidc/callback" ];
         })
         (mkClient config.site.apps.immich {
-          client_secret = "$argon2id$v=19$m=65536,t=3,p=4$MZZGRuALuFh6qmXYhBFRTg$27qX6xv264Cs6cuj18afQa1oc4ddt/X+ndYBkDVcMVU";
           redirect_paths = [ "auth/login" "user-settings" ];
           redirect_uris = [ "app.immich:///oauth-callback" ];
           response_types = [ "code" ];
@@ -157,36 +156,30 @@ in {
           token_endpoint_auth_method = "client_secret_post";
         })
         (mkClient config.site.apps.jellyfin {
-          client_secret = "$argon2id$v=19$m=65536,t=3,p=4$LuRHBAoKS3llgeNibFAJCg$fqaNlYvfRKiGdbVUOi89pXeWQwe5pStzNhrvmvvJrWc";
           redirect_paths = [ "sso/OID/redirect/authelia" ];
           authorization_policy = "one_factor";
           token_endpoint_auth_method = "client_secret_post";
         })
         (mkClient config.site.apps.mealie {
-          client_secret = "$argon2id$v=19$m=65536,t=3,p=4$hTubW+z8HklfQlm2mi8oPA$cFVnkx8aYkDkPlSJUcHo5F88vCfN/ija/U44sEqOa64";
           authorization_policy = "one_factor";
           redirect_paths = [ "login" ];
           redirect_uris = [ "http://localhost:${toString config.site.apps.mealie.port}/login" ];
         })
         (mkClient config.site.apps.memos {
-          client_secret = "$argon2id$v=19$m=65536,t=3,p=4$5SHxB5qqWhPiYFeZ/cUXQQ$u1lemwNPR6FCopfiR65/jAt0DOfa5GXeKd/YqkD8l7M";
           redirect_paths = [ "auth/callback" ];
           require_pkce = false;
           pkce_challenge_method = "";
           token_endpoint_auth_method = "client_secret_post";
         })
         (mkClient config.site.apps.paperless {
-          client_secret = "$argon2id$v=19$m=65536,t=3,p=4$eqYYBvna/cLi1FZ8nE+AVg$6fZX2RlCRTgJwBAR1fLwq2fCytjSheS5AJ0uDB9Rkwk";
           redirect_paths = [ "accounts/oidc/authelia/login/callback/" ];
         })
         (mkClient config.site.apps.sftpgo {
           authorization_policy = "one_factor";
-          client_secret = "$argon2id$v=19$m=65536,t=3,p=4$XY2jSGxOVxEMvFvJ4hQ1Fg$hZah2TkuBoQrlJi8wiO5oyMGh3y09nc1CPr9UHbe/7k";
           claims_policy = "sftpgo";
           redirect_paths = [ "web/oidc/redirect" "web/oauth2/redirect" ];
         })
         (mkClient config.site.apps.vikunja {
-          client_secret = "$argon2id$v=19$m=65536,t=3,p=4$zRMdh029w57vBVKYJUbrOA$XpthqZlqEa6neEoIffR8wHEt++KuMykATd/tte//4II";
           redirect_paths = [ "auth/openid/authelia" ];
         })
       ];
