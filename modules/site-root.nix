76a0c7465562f8cf80eb898fdd2ec2b6a58f3246
TEC <git@tecosaur.net>
2026-04-27 14:05:58 +0800

    Fancier services/welcome pages using OIDC info

--- modules/site-root.nix
+++ modules/site-root.nix
@@ -5,19 +5,33 @@ with lib;
 let
   mkAppExport = name: app: let
     prefix = lib.toUpper (lib.replaceStrings [ "-" ] [ "_" ] name);
+    allGroups = [ app.groups.primary ] ++                                                                                                              
+                (lib.optional (app.groups.admin != null) app.groups.admin) ++                                                                                    
+                app.groups.extra;  
+    groupChecks =  "(any " + (lib.concatStringsSep " " (map (g: ''(contains "${g}" $groups)'') allGroups)) + ")";
   in
-    ''
-    export ${prefix}_SUBDOMAIN="${app.subdomain}";
-    export ${prefix}_URL="${app.subdomain}.${config.site.domain}";
-    '';
-  mkAppExports = apps: lib.concatStringsSep "\n"
-    (lib.attrsets.mapAttrsToList mkAppExport
-      (lib.filterAttrs (_: v: v.enabled) apps));
-  apps-exports = mkAppExports config.site.apps;
+    if app.enabled then
+      {
+        "${prefix}_SUBDOMAIN" = app.subdomain;
+        "${prefix}_URL" = "${app.subdomain}.${config.site.domain}";
+        "${prefix}_ACCESS" = "(or (not $groups) ${groupChecks})";
+        "${prefix}_ENABLED" = "true";
+      } else
+        {
+          "${prefix}_SUBDOMAIN" = "<unknown>";
+          "${prefix}_URL" = "${config.site.domain}";
+          "${prefix}_ACCESS" = "false";
+          "${prefix}_ENABLED" = "false";
+        };
+  apps-exports = lib.concatMapAttrs mkAppExport config.site.apps;
+  apps-env-export = lib.concatStringsSep "\n" (lib.mapAttrsToList
+    (key: val: "export ${key}='${val}'")
+    apps-exports);
+  app-env-vars = lib.concatStringsSep " " (lib.mapAttrsToList (k: _: "\$${k}") apps-exports);
   static-root = pkgs.runCommand "static-root" { buildInputs = [ pkgs.gettext ]; } ''
       export DOMAIN='${config.site.domain}'
       export ACCENT='${config.site.accent.primary}'
-      ${apps-exports}
+      ${apps-env-export}
       export APPS_TEXT=$(cat <<'HEREDOC'
       ${concatStringsSep "\n" (map (app: "• ${app.name} (${app.description})")
         (builtins.filter (app: app.enabled) (builtins.attrValues config.site.apps)))}
@@ -27,7 +41,7 @@ let
       apply_template() {
           for file in "$@"; do
               tmpout=$(mktemp)
-              envsubst < "$file" > "$tmpout"
+              envsubst '$DOMAIN $ACCENT $APPS_TEXT $WELCOME ${app-env-vars}' < "$file" > "$tmpout"
               mv "$tmpout" "$file"
           done
       }
@@ -54,14 +68,18 @@ in {
         }
         @browser header User-Agent *Mozilla*
         try_files {path} {path}.html
+        templates
         root ${static-root}
         route {
             handle /reload {
                 header Clear-Site-Data "\"cache\""
                 redir * /
             }
-            file_server @browser-auth {
-                index index-private.html
+            handle @browser-auth {
+                import auth
+                file_server {
+                    index index-private.html
+                }
             }
             file_server @browser {
                 index index-public.html
